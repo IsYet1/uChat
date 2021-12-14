81dba03d855dbe7b155e3457470dc75b5f7dd3db
IsYet1 <Don@IsYet.com>
2021-12-13 19:52:29 -0600

    Initial Commit

---
+++ uChat/uChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  uChatApp.swift
+//  uChat
+//
+//  Created by Don McKenzie on 13-Dec-21.
+//
+
+import SwiftUI
+
+@main
+struct uChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
